547b5cd75b118ae1c5ecb9f9efd5f4d84c50f2ad
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2020-01-15 07:09:05 -0800

    Revert "create and test functions"
    This reverts commit c9b3b94a2396497e6a63c115abe9ed1c1171879f.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,18 +2,16 @@ module.exports = {
   add,
   subtract,
   multiply
-};
-
-function add(a = 0, b = 0) {
-  return a + b;
 }
 
-function subtract(a = 0, b = 0) {
-  return a - b;
+function add(params) {
+  
 }
 
-function multiply(a, b) {
-  if (!a && !b) return 0;
-  if (!b) return a;
-  return a * b;
+function subtract(params) {
+  
+}
+
+function multiply(params) {
+  
 }
