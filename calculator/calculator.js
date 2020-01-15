517e09f84133453c57c76c9c695c556845774b42
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2020-01-15 07:05:14 -0800

    create and test functions

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,16 +2,18 @@ module.exports = {
   add,
   subtract,
   multiply
+};
+
+function add(a = 0, b = 0) {
+  return a + b;
 }
 
-function add(params) {
-  
+function subtract(a = 0, b = 0) {
+  return a - b;
 }
 
-function subtract(params) {
-  
-}
-
-function multiply(params) {
-  
+function multiply(a, b) {
+  if (!a && !b) return 0;
+  if (!b) return a;
+  return a * b;
 }
