75f77bb0a796ac030d486475d1219d465487b2dd
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2020-01-14 20:37:17 -0800

    add calculator pseudocode

---
+++ calculator/calculator.test.js
@@ -0,0 +1,11 @@
+const calculator = require('./calculator');
+
+// what should the add function do?
+
+// take two values and combine them
+// return one value
+// add(2,2) => 4
+
+// what should happen on no arguments?
+// what about one argument?
+// what about many?
